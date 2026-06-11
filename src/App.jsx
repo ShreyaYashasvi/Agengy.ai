ceb265e0713b0c9a2b5feaab86ed6c5f72e05b10
ShreyaYashasvi <shreyasvi314@gmail.com>
2026-06-12 01:40:33 +0530

    component making

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React ,{useState}from 'react'
 import Nav from './Componets/Nav'
+import Hero from './Componets/Hero'
+import TrustedBy from './Componets/TrustedBy'
+import Services from './Componets/Services'
 
 const App = () => {
-    const[theme,setTheme]=useState('light')
+    const[theme,setTheme]=useState(localStorage.getItem('theme')?localStorage.getItem('theme'):'light')
   return (
-    <div className=' dark relative min-h-screen'>
+    <div className=' dark: relative min-h-screen'>
       <Nav theme={theme} setTheme={setTheme}/>
+      <Hero/>
+      <TrustedBy/>
+      <Services/>
     </div>
   )
 }
